7e208d109815bd9e2e86614b14d9cc8b87c565a3
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-02-26 20:10:45 +0200

    Teodor Dichev | Week 02 | Task 03 | Fix

--- Practicum/Pract. 02/Task03.cpp
+++ Practicum/Pract. 02/Task03.cpp
@@ -9,7 +9,7 @@ bool isWhiteSpace(char ch) {
     return ch == ' ' || ch == '\t' || ch == '\n';
 }
 
-size_t getFileSize(const char * fileName) {
+size_t getNumbersCount(const char * fileName) {
     std::ifstream ifs(fileName);
     size_t size = 0;
     char ch;
@@ -33,7 +33,7 @@ size_t getFileSize(const char * fileName) {
     return size;
 }
 
-void saveArrToFile(const char * fileName, size_t size, const int * arr) {
+void clearFileContent(const char * fileName) {
     std::ofstream ofs(fileName);
 
     if (!ofs.is_open()) {
@@ -41,16 +41,8 @@ void saveArrToFile(const char * fileName, size_t size, const int * arr) {
         return;
     }
 
-    if (!arr) { // nullptr check
-        ofs.seekp(std::ios::out | std::ios::trunc); // Clear file
-        return;
-    }
-    
-    for (size_t i = 0; i < size; i++) {
-        ofs << arr[i] << " ";
-    }
-    
-    delete [] arr; // Deleting the "inhereted" array
+    ofs.seekp(std::ios::out | std::ios::trunc); // Clear file
+
 }
 
 bool ascending(int a, int b) {
@@ -61,45 +53,27 @@ bool descending(int a, int b) {
     return a > b;
 }
 
-int * copyArr(const int * src, size_t size) {
-    if (!src) { // nullptr check
-        return nullptr;
-    }
-
-    int * res = new int[size];
-    for (size_t i = 0; i < size; i++) {
-        res[i] = src[i];
+void selectionSort(int * arr, size_t size, bool (*predicate)(int, int)) {
+    if (size == 0 || !arr) { 
+        return;
     }
     
-    return res;
-}
-
-// We want to keep the original array so we return a new one
-int * selectionSort(const int * arr, size_t size, bool (*predicate)(int, int)) {
-    if (size == 0) { 
-        return nullptr;
-    }
-    
-    int * res = copyArr(arr, size);
-
     for (size_t i = 0; i < size - 1; i++) {
         size_t bestIndex = i;
 
         for (size_t j = i + 1; j < size; j++) {
-            if(predicate(res[j], res[bestIndex])) {
+            if(predicate(arr[j], arr[bestIndex])) {
                 bestIndex = j;
             }
         }
         
         if (bestIndex != i) {
-            std::swap(res[i], res[bestIndex]);
+            std::swap(arr[i], arr[bestIndex]);
         }
     }
-
-    return res;
 }
 
-void readAndSortFile(const char * fileName) {
+void readFileContent(const char * fileName, int * arr, size_t size) {
     std::ifstream ifs(fileName);
 
     if (!ifs.is_open()) {
@@ -107,21 +81,41 @@ void readAndSortFile(const char * fileName) {
         return;
     }
 
-    size_t size = getFileSize(fileName);
-    int * arr = new int[size];
-
     for (size_t i = 0; i < size; i++) {
         ifs >> arr[i];
     }
+}
 
-    saveArrToFile("ascending.txt", size, selectionSort(arr, size, ascending));
-    saveArrToFile("descending.txt", size, selectionSort(arr, size, descending));
-
-    delete [] arr;
+void saveFileContent(const char * fileName, const int * arr, size_t size) {
+        std::ofstream ofs(fileName);
+    
+        if (!ofs.is_open()) {
+            std::cout << "Something went wrong";
+            return;
+        }
+    
+        if (!arr) { // nullptr check
+            clearFileContent(fileName);
+            return;
+        }
+        
+        for (size_t i = 0; i < size; i++) {
+            ofs << arr[i] << " ";
+        }
 }
 
 // Create this file so that the program runs properly
 // Tested with: 9 5 0 1 2 4, ,5 5 -5 5 -5, 101 102 -103
 int main() {
-    readAndSortFile("file03.txt");
+    size_t size = getNumbersCount("file03.txt");
+    int * arr = new int[size];
+    readFileContent("file03.txt", arr, size);
+    
+    selectionSort(arr, size, ascending);
+    saveFileContent("ascending.txt", arr, size);
+
+    selectionSort(arr, size, descending);
+    saveFileContent("descending.txt", arr, size);
+
+    delete [] arr;
 }
